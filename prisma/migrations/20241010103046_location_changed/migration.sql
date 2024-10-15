b9310d2c0111e4dcb7876f5f449c65522d8edbc0
Sahil Maliya <sahilmaliya55@gmail.com>
2024-10-15 23:18:37 +0530

    Delete prisma/migrations directory

--- prisma/migrations/20241010103046_location_changed/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "location_user_id_isDefault_key";
--- prisma/migrations/20241011045927_property_date/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "property" ADD COLUMN     "listedon" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
